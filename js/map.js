b01fde176a341e3d09bbb3e42d9e4c6a72cf3879
olmo <olmo1995@gmail.com>
2021-12-24 17:15:11 +0100

    feat:added map

---
+++ js/map.js
@@ -0,0 +1,34 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoib2xtbzk1IiwiYSI6ImNreGtqd3c3aTE3YzYycXViMW1xZ2NwNHcifQ.-6KPm5u6FHFcoCkl0UuLIw';
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {enableHighAccuracy: true})
+
+function successLocation(position) {
+    setupMap([position.coords.longitude, position.coords.latitude])
+  }
+  
+  function errorLocation() {
+    setupMap([-2.24, 53.48])
+  }
+  
+  function setupMap(center) {
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: center,
+      zoom: 16
+    })
+  
+    const nav = new mapboxgl.NavigationControl()
+    map.addControl(nav)
+  
+    var directions = new MapboxDirections({
+      accessToken: mapboxgl.accessToken
+    })
+  
+    map.addControl(directions, "top-left")
+  }
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11'
+});
