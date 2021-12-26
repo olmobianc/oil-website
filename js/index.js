7768605d9bd01edff0d64d9d4eb3c9432e72db66
olmo <olmo1995@gmail.com>
2021-12-26 20:01:16 +0100

    added preventivo in all sections

--- js/index.js
+++ js/index.js
@@ -11,3 +11,45 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     })
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("scroll", function() {
+        const carouselContainerSection = document.querySelector("#carousel-container");
+        //get the preventivo section height
+        const carouselContainerHeight = carouselContainerSection.offsetTop;
+        //get the scrolling height
+        const windowHeight = window.innerHeight + window.scrollY;
+        //when user reaches the preventivo height
+        if(windowHeight >= carouselContainerHeight) {
+            carouselContainerSection.classList.add("preventivo-comes-up")
+        }
+    })
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("scroll", function() {
+        const landSection = document.querySelector("#land");
+        //get the preventivo section height
+        const landHeight = landSection.offsetTop;
+        //get the scrolling height
+        const windowHeight = window.innerHeight + window.scrollY;
+        //when user reaches the preventivo height
+        if(windowHeight >= landHeight) {
+            landSection.classList.add("preventivo-comes-up")
+        }
+    })
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("scroll", function() {
+        const productionSection = document.querySelector("#production");
+        //get the preventivo section height
+        const productionHeight = productionSection.offsetTop;
+        //get the scrolling height
+        const windowHeight = window.innerHeight + window.scrollY;
+        //when user reaches the preventivo height
+        if(windowHeight >= productionHeight) {
+            productionSection.classList.add("preventivo-comes-up")
+        }
+    })
+})
