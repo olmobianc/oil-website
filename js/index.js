c6204f7af18b2f306f968612320ec541164d61bf
olmo <olmo1995@gmail.com>
2022-01-04 19:22:13 +0100

    fix:fixed mobile view homepage navbar

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
+//FUNCTION TO OPEN MENU ON MOBILE VIEW
+const burger = document.querySelector("#burger")
+const firstLine = document.querySelector("#first-line")
+const secondLine = document.querySelector("#second-line")
+const thirdLine = document.querySelector("#third-line")
+const hiddenMenu = document.querySelector(".is-hidden")
+document.addEventListener("DOMContentLoaded", () => {
+    burger.addEventListener("click", () => {
+        if(hiddenMenu.classList.contains("is-hidden")) {
+            hiddenMenu.classList.remove("is-hidden")
+            firstLine.classList.add("rotate-right")
+            secondLine.classList.add("fade-out")
+            thirdLine.classList.add("rotate-left")
+        } else {
+            hiddenMenu.classList.add("is-hidden")
+            firstLine.classList.remove("rotate-right")
+            secondLine.classList.remove("fade-out")
+            thirdLine.classList.remove("rotate-left")
+        }
+    })
+})
+
 document.addEventListener("DOMContentLoaded", function() {
     document.addEventListener("scroll", function() {
         const preventivoSection = document.querySelector("#preventivo");
