e3dd235230e6dc7a697a444bb1aae928173be96c
olmo <olmo1995@gmail.com>
2022-01-05 13:08:24 +0100

    fix:fixed script carousel on left arrow mobile

--- js/carousel.js
+++ js/carousel.js
@@ -1,22 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
     const rightArrow = document.querySelector(".fa-angle-right")
+    const leftArrow = document.querySelector(".fa-angle-left")
     const carouselImages = document.querySelectorAll(".carousel-image")
 
     let counter = 0
 
+    leftArrow.style.color = "grey"
+    leftArrow.style.cursor = "default"
+
     //MOBILE
     if(window.innerWidth <= 970) {
-        console.log("ciao")
         for(let i = 1; i < carouselImages.length; i++) {
             carouselImages[i].classList.add("invisible")
         }
     }
     
+    //RIGHT CLICK
     rightArrow.addEventListener("click", () => {
+
+        leftArrow.style.color = "#7f6459"
+        leftArrow.style.cursor = "pointer"
+
         //MOBILE
         if(window.innerWidth <= 970) {
-            console.log("ciao")
             counter++
+            console.log(counter)
             for(let i = counter - 1; i < counter; i++) {
                 if(i >= carouselImages.length) {
                     rightArrow.style.color = "grey"
@@ -56,4 +64,24 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         }
     })
+
+    //LEFT CLICK
+    leftArrow.addEventListener("click", () => {
+        //MOBILE
+        if(window.innerWidth <= 970) {
+            if(counter != 0) {
+                carouselImages[counter].classList.add("invisible")
+                const lastElement = document.querySelectorAll(".carousel-image")[carouselImages.length - 1]
+                lastElement.classList.remove("invisible")
+                document.querySelector("#test").insertBefore(lastElement, document.querySelector("#test").firstChild);
+                counter--
+                console.log(counter)
+            } else {
+                leftArrow.style.color = "grey"
+                leftArrow.style.cursor = "default"
+            }
+            
+        }
+    })
+
 })
